303a135f616ee7f72642366cccea32482c495946
trish1221 <trish.122021@gmail.com>
2022-04-25 17:23:34 -0700

    Step4

--- main.py
+++ main.py
@@ -106,18 +106,19 @@ def go(config: DictConfig):
                     "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config":rf_config,
                     "max_tfidf_features":config["modeling"]["max_tfidf_features"],
-                    "output_artifact": "random_forest_export.csv"
+                    "output_artifact": "random_forest_export"
                 },
             )
         
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 if __name__ == "__main__":
     go()
